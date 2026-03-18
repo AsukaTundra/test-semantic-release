881da4f89ebcfea88fea9cddf931894513b6829c
Мингиян <vodopyanov_cp@etm.ru>
2026-03-18 08:37:44 +0300

    feat(semantic): test2

--- src/mock.ts
+++ src/mock.ts
@@ -21,3 +21,11 @@ export const mockArray = [
     address: "123 Main St, Anytown, USA",
   },
 ];
+
+export const mockObject = {
+  name: "John Doe",
+  age: 30,
+  email: "john.doe@example.com",
+  phone: "1234567890",
+  address: "123 Main St, Anytown, USA",
+};
